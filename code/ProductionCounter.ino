af65f1e1cef1ac13c19811bdb8d55ecc9b24ca93
pauliendoms <r0810768@student.thomasmore.be>
2021-04-30 08:30:23 +0200

    setup temp sensor toegevoegd

--- code/ProductionCounter.ino
+++ code/ProductionCounter.ino
@@ -18,8 +18,8 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 /* ultrasonic ---------------- */
 
-#define EchoPin 10
-#define TrigPin 11
+#define ECHO_PIN 10
+#define TRIG_PIN 11
 
 int duration;
 
@@ -37,11 +37,21 @@ LiquidCrystal_I2C lcd(0x27,16,2);
  * LCD VCC ->  5V
  */
 
-/* --------------------------- */
+/* temperature sensor ----------------- */
+#define TEMP_PIN A3
+
+float temp;
+float waarde;
+float spanning;
+
+/* ------------------------------------ */
+
+
 
 void setup(){
-    pinMode(TrigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-    pinMode(EchoPin, INPUT);
+    pinMode(TRIG_PIN, OUTPUT); // Sets the trigPin as an OUTPUT
+    pinMode(ECHO_PIN, INPUT);
+    // pinMode(TEMP_PIN, INPUT) -> ben niet zeker of dit erbij moet, eens testen of het werkt zonder
     Serial.begin(9600);
 
     lcd.init();
@@ -61,12 +71,12 @@ void loop() {
 
     /* ULTROSONIC TEST */
 
-    digitalWrite(TrigPin, LOW);
+    digitalWrite(TRIG_PIN, LOW);
     delayMicroseconds(2);
-    digitalWrite(TrigPin, HIGH);
+    digitalWrite(TRIG_PIN, HIGH);
     delayMicroseconds(10);
-    digitalWrite(TrigPin, LOW);
-    duration = pulseIn(EchoPin, HIGH);
+    digitalWrite(TRIG_PIN, LOW);
+    duration = pulseIn(ECHO_PIN, HIGH);
     delay(1000);
     lcd.clear();
     lcd.print(duration);
